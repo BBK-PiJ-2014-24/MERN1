c8f9014de491a5f2f1347fb32f8ac10e15800e12
Stewart Newnham <git@newnham.com>
2021-07-31 23:02:33 +0800

    Added ImageUpload element to post image to user file

--- frontend/src/places/pages/Auth/Auth.js
+++ frontend/src/places/pages/Auth/Auth.js
@@ -1,19 +1,21 @@
-import React, { useState, useContext } from 'react'
-import Card from '../../../shared/components/UIElements/Card';
-import Input from '../../../shared/components/FormElements/Input';
-import Button from '../../../shared/components/FormElements/Button';
-import ErrorModal from '../../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../../shared/components/UIElements/LoadingSpinner';
-import { useHttpClient } from '../../../shared/hooks/http-hook';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../../shared/util/validators';
+import React, { useState, useContext } from "react";
+import Card from "../../../shared/components/UIElements/Card";
+import Input from "../../../shared/components/FormElements/Input";
+import Button from "../../../shared/components/FormElements/Button";
+import ErrorModal from "../../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../../shared/hooks/http-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../../shared/util/validators";
 
-import useForm from '../../../shared/hooks/form-hook';
-import { AuthContext } from '../../../shared/context/auth-context';
+import useForm from "../../../shared/hooks/form-hook";
+import ImageUpload from "../../../shared/components/FormElements/ImageUpload";
+import { AuthContext } from "../../../shared/context/auth-context";
 
-import './Auth.css';
-
-
-
+import "./Auth.css";
 
 const Auth = () => {
   const auth = useContext(AuthContext);
@@ -23,13 +25,13 @@ const Auth = () => {
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
-        value: '',
-        isValid: false
+        value: "",
+        isValid: false,
       },
       password: {
-        value: '',
-        isValid: false
-      }
+        value: "",
+        isValid: false,
+      },
     },
     false
   );
@@ -39,7 +41,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -48,47 +51,51 @@ const Auth = () => {
         {
           ...formState.inputs,
           name: {
-            value: '',
-            isValid: false
-          }
+            value: "",
+            isValid: false,
+          },
+          image: {
+            value: null,
+            isValid: false,
+          },
         },
         false
       );
     }
-    setIsLoginMode(prevMode => !prevMode);
+    setIsLoginMode((prevMode) => !prevMode);
   };
 
-  const authSubmitHandler = async event => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
+    console.log(formState.inputs);
 
     if (isLoginMode) {
       try {
         const data = await sendRequest(
-          'http://localhost:5000/api/users/login',
-          'POST',
+          "http://localhost:5000/api/users/login",
+          "POST",
           JSON.stringify({
             email: formState.inputs.email.value,
-            password: formState.inputs.password.value
+            password: formState.inputs.password.value,
           }),
           {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           }
         );
         auth.login(data.user.id);
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData(); // part of the built in DOM functionality
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
+
         const data = await sendRequest(
-          'http://localhost:5000/api/users/signup',
-          'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {
-            'Content-Type': 'application/json'
-          }
+          "http://localhost:5000/api/users/signup",
+          "POST",
+          formData
         );
 
         auth.login(data.user.id);
@@ -115,6 +122,9 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && (
+            <ImageUpload id="image" center onInput={inputHandler} />
+          )}
           <Input
             element="input"
             id="email"
@@ -134,11 +144,11 @@ const Auth = () => {
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
-            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+            {isLoginMode ? "LOGIN" : "SIGNUP"}
           </Button>
         </form>
         <Button inverse onClick={switchModeHandler}>
-          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
         </Button>
       </Card>
     </React.Fragment>
---
+++ frontend/src/shared/components/FormElements/ImageUpload.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useState, useEffect } from "react";
+import Button from "./Button";
+
+import "./ImageUpload.css";
+
+const ImageUpload = (props) => {
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
+
+  const filePickerRef = useRef();
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  const pickHandler = (e) => {
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (e.target.files && e.target.files.length === 1) {
+      pickedFile = e.target.files[0];
+      setFile(pickedFile);
+      setIsValid(true);
+      fileIsValid = true;
+    } else {
+      setIsValid(false);
+      fileIsValid = false;
+    }
+    props.onInput(props.id, pickedFile, fileIsValid);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader(); //embedded obj in DOM
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
+
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        ref={filePickerRef}
+        style={{ display: "none" }}
+        type="file"
+        accept=".jpg, .png, .jpeg"
+        onChange={pickHandler}
+      />
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+          {previewUrl && <img src={previewUrl} alt="Preview" />}
+          {!previewUrl && <p>Select Image</p>}
+        </div>
+        <Button type="button" onClick={pickImageHandler}>
+          Select Image
+        </Button>
+      </div>
+      {!isValid && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default ImageUpload;
