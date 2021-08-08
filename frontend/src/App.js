5c191049c446496776071f2a2648c9c0d19b1ec8
Stewart Newnham <git@newnham.com>
2021-08-08 19:18:58 +0800

    reodered imports so that lazy is at the end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+import './App.css';
 
 /// LAZY LOADS
 const Users = React.lazy(()=> import('./user/pages/Users'));
@@ -11,8 +13,6 @@ const UserPlaces = React.lazy(()=> import('./places/pages/UserPlaces/UserPlaces'
 const UpdatePlace = React.lazy(()=> import('./places/pages/UpdatePlace/UpdatePlace'));
 const Auth = React.lazy(()=> import('./places/pages/Auth/Auth'));
 
-import './App.css';
-import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 
 
 function App() {
